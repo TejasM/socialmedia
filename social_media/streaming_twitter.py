9fa8ae550e60cdc577c15ae3f37563c76b42a9b0
Tejas Mehta <tejasmehta0@gmail.com>
2014-11-29 18:30:27 -0500

    done streaming

--- social_media/streaming_twitter.py
+++ social_media/streaming_twitter.py
@@ -1,3 +1,5 @@
+from dashboard.models import Tweet, TwitterFollower, UserSpec
+
 __author__ = 'tmehta'
 from twython import TwythonStreamer
 
@@ -6,6 +8,26 @@ class MyStreamer(TwythonStreamer):
     def on_success(self, data):
         if 'text' in data:
             print data['text'].encode('utf-8')
+            user_id = data['user']['id']
+            try:
+                follower = TwitterFollower.objects.get(twitter_id=user_id)
+            except TwitterFollower.DoesNotExist:
+                follower = TwitterFollower.objects.create(twitter_id=user_id, handle=data['user']['screen_name'],
+                                                          name=data['user']['name'],
+                                                          followers_count=data['user']["followers_count"],
+                                                          friends_count=data['user']["friends_count"],
+                                                          description=data['user']["description"],
+                                                          timezone=data['user']['timezone'])
+            spec = None
+            for tag in data['hashtags']:
+                try:
+                    spec = UserSpec.objects.get(hash_tag=tag['text'])
+                except:
+                    pass
+            if spec:
+                Tweet.objects.create(text=data['text'], retweets=data['retweet_count'], tweeted_at=data["created_at"],
+                                     tweet_id=data['id'], by=follower, spec=spec)
+                print "Tweet created"
 
     def on_error(self, status_code, data):
         print status_code
